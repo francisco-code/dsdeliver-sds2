d99d4464d1ab8512630265fb95e889d24b20f4eb
Felipe Correia <felipedreamcorreia@hotmail.com>
2021-01-10 02:48:01 -0300

    Add Footer

---
+++ front-web/src/Footer/index.tsx
@@ -0,0 +1,25 @@
+import './styles.css';
+import { ReactComponent as YouTubeIcon } from './youtube.svg';
+import { ReactComponent as LinkedInIcon } from './linkedin.svg';
+import { ReactComponent as InstagramIcon } from './instagram.svg';
+
+function Footer() {
+    return (
+        <footer className="main-footer">
+            App desenvolvido durante a 2ª ed. do evento Semana DevSuperior
+            <div className="footer-icons">
+                <a href="https://www.youtube.com/channel/UC6f7eN1TUIqztgjcsJxgsoA" target="_new">
+                    <YouTubeIcon />
+                </a>
+                <a href="https://www.linkedin.com/in/felipe-correia-08b50147/" target="_new">
+                    <LinkedInIcon />
+                </a>
+                <a href="https://www.instagram.com/felip.correia/?hl=pt-br" target="_new">
+                    <InstagramIcon />
+                </a>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
