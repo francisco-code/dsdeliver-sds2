6b1b5b77415ee5ec60c02091b1495a6964f5c0a5
Felipe Correia <felipedreamcorreia@hotmail.com>
2021-01-10 23:05:42 -0300

    Finish front-end-web

--- front-web/src/Footer/index.tsx
+++ front-web/src/Footer/index.tsx
@@ -6,7 +6,7 @@ import { ReactComponent as InstagramIcon } from './instagram.svg';
 function Footer() {
     return (
         <footer className="main-footer">
-            App desenvolvido durante a 2ª ed. do evento Semana DevSuperior
+            App desenvolvido por Francisco Felipe de Brito Correia durante a 2ª ed. do evento Semana DevSuperior
             <div className="footer-icons">
                 <a href="https://www.youtube.com/channel/UC6f7eN1TUIqztgjcsJxgsoA" target="_new">
                     <YouTubeIcon />
